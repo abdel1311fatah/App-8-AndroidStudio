cf251f6b5aea1304473243ceb5c28ed7f6131944
aibarra-vegga <aibarra-ext@veggadigital.com>
2023-11-13 14:57:05 +0100

    Acabat

--- app/src/main/java/com/example/app8_2023_24/MainActivity.java
+++ app/src/main/java/com/example/app8_2023_24/MainActivity.java
@@ -15,13 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView appname;
     private TextView resultat;
-    private Button reset;
-    private Button sumar;
-    private Button restar;
-    private Button multi;
-    private Button div;
-    private Button igual;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
         appname = (TextView) findViewById(R.id.appname);
         appname.setText("App8_2023_24");
         resultat = (TextView) findViewById(R.id.resultat);
-        reset = (Button) findViewById(R.id.btnReset);
     }
 
     public void numeros(View view) {
@@ -40,42 +32,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void operadors(View view) {
-
         Button btn = (Button) view;
+        String text = resultat.getText().toString();
 
-        if (!(resultat.getText().toString().contains("+") || resultat.getText().toString().contains("-") || resultat.getText().toString().contains("*") || resultat.getText().toString().contains("/"))) {
-            resultat.setText(resultat.getText().toString() + btn.getText().toString());
+        if (!(text.contains("+") || text.contains("-") || text.contains("*") || text.contains("/"))) {
+            resultat.setText(text + btn.getText().toString());
         } else {
-            char[] texte = new char[resultat.getText().toString().length()];
-            texte = resultat.getText().toString().toCharArray();
-            for (int i = 0; i < resultat.getText().toString().length(); i++) {
-                if (texte[i] == '+' || texte[i] == '-' || texte[i] == '*' || texte[i] == '/') {
-                    texte[i] = btn.getText().toString().charAt(0);
-                }
-            }
+            char operator = btn.getText().toString().charAt(0);
+            String updatedText = text.substring(0, text.length() - 1) + operator;
+            resultat.setText(updatedText);
         }
-
     }
 
     public void igual(View view) {
-        String expresion = resultat.getText().toString();
+        String operacio = resultat.getText().toString();
 
-        if (expresion.contains("+")) {
-            realitzarOperacio(expresion, '+');
-        } else if (expresion.contains("-")) {
-            realitzarOperacio(expresion, '-');
-        } else if (expresion.contains("*")) {
-            realitzarOperacio(expresion, '*');
-        } else if (expresion.contains("/")) {
-            realitzarOperacio(expresion, '/');
+        if (operacio.contains("+")) {
+            realitzarOperacio(operacio, '+');
+        } else if (operacio.contains("-")) {
+            realitzarOperacio(operacio, '-');
+        } else if (operacio.contains("*")) {
+            realitzarOperacio(operacio, '*');
+        } else if (operacio.contains("/")) {
+            realitzarOperacio(operacio, '/');
         } else {
            resultat.setText("No hi ha operador");
         }
     }
 
-    private void realitzarOperacio(String expresion, char operador) {
+    private void realitzarOperacio(String operacio, char operador) {
         String operadorString = Character.toString(operador);
-        String[] numeros = expresion.split(operadorString);
+        String[] numeros = operacio.split("\\" + operadorString);
         long resultatOperacio = 0;
 
         if (numeros.length != 2) {
@@ -98,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     if (n2 != 0) {
                         resultatOperacio = n1 / n2;
                     } else {
-                        resultat.setText("Cannot divide by 0");
+                        resultat.setText("No es pot dividir per 0");
                         return;
                     }
                     break;
